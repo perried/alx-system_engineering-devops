51e5d0d08727d0694dc630ca0ccbe5eedd0ecf48
perrie <periedamas203@gmail.com>
2023-06-23 13:33:02 -0700

    Done with task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Declare a file resource for school
+
+file { '/tmp/school':
+    ensure => present,
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
+    content => 'I love Puppet',
+}

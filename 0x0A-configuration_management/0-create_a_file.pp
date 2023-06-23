4e8188c4784b702e2f3f3e80da579513ec2856b0
perrie_d <111010793+perried@users.noreply.github.com>
2023-06-23 22:41:58 +0200

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #Declare a file resource for school
 file { '/tmp/school':
-    ensure => present,
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
